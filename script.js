eeeadc2f4931c7b1c997907b62b19cf8fa4a7209
empirical72-dev <empirical72@gmail.com>
2026-07-08 13:47:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -148,16 +148,3 @@ function animateSpin() {
   ctx.restore();
 
   if (spinVelocity > 0.002) {
-    requestAnimationFrame(animateSpin);
-  } else {
-    spinning = false;
-    showResult();
-  }
-}
-
-// 결과 판정 (포인터 위쪽 기준, 아래를 향하는 화살표)
-function showResult() {
-  const numOptions = options.length;
-  const arc = 2 * Math.PI / numOptions;
-  const adjustedAngle = (2 * Math.PI - (angle % (2 * Math.PI))) % (2 * Math.PI);
-  const selectedIndex = Math
